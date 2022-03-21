60d9acc857c4654bef522bc119ac7197216c4cb8
Anton Kuvshinov <swwer82@gmail.com>
2022-03-21 18:18:25 +0300

    connect_ok

--- Program.cs
+++ Program.cs
@@ -1,8 +1,27 @@
-﻿namespace HelloWorld{
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sharp7;
+
+
+
+
+namespace Snap7Exemple{
 
     class Program{
         static void Main(string[]args){
-            Console.WriteLine("Hello world");
+            var client = new S7Client();
+            int connectionResult = client.ConnectTo("192.168.149.58",0,2);
+            if(connectionResult==0){
+                Console.WriteLine("Connection ok!");
+            }
+            else{
+                Console.WriteLine("Connection error!");
+                return;
+            }
+            client.Disconnect();
         }
     }
 }
