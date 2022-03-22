ecbcc99675504cf786393c10f499364204b83359
Anton Kuvshinov <swwer82@gmail.com>
2022-03-22 16:21:13 +0300

    change_name_library

--- Program.cs
+++ Program.cs
@@ -32,19 +32,19 @@ namespace Sharp7Exemple
                 Console.WriteLine("Error: " + client.ErrorText(result));
             }
             int db2dbw2 = S7.GetIntAt(db2Buffer, 2);
-            Console.WriteLine("db2.DBW2: " + db2dbw2);
+            Console.WriteLine("DB2.DBW2: " + db2dbw2);
 
             double db2ddd4 = S7.GetRealAt(db2Buffer, 4);
-            Console.WriteLine("db2.DBD4: " + db2ddd4);
+            Console.WriteLine("DB2.DBD4: " + db2ddd4);
 
             double db2dbd8 = S7.GetDIntAt(db2Buffer, 8);
-            Console.WriteLine("db2.DBD8: " + db2dbd8);
+            Console.WriteLine("DB2.DBD8: " + db2dbd8);
 
             double db2dbd12 = S7.GetDWordAt(db2Buffer, 12);
-            Console.WriteLine("db2.DBD12: " + db2dbd12);
+            Console.WriteLine("DB2.DBD12: " + db2dbd12);
 
             double db2dbw16 = S7.GetWordAt(db2Buffer, 16);
-            Console.WriteLine("db2.DBD16: " + db2dbw16);
+            Console.WriteLine("DB2.DBD16: " + db2dbw16);
             // Disconnect the client
             client.Disconnect();
         }
